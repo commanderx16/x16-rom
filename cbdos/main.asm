eaa769d778416c87ac533600f3c64085bddc053d
Michael Steil <mist64@mac.com>
2019-08-26 18:48:43 -0700

    ...

--- cbdos/main.asm
+++ cbdos/main.asm
@@ -22,9 +22,10 @@
 .include "65c02.inc"
 
 NUM_BUFS = 4
+FN_BUFNO = NUM_BUFS ; filename buffer follows general purpose buffers
 DIRSTART = $0801
 
-curbuf     = 4 ; 2 byte
+buffer = 4 ; 2 byte
 
 .segment "cbdos_data"
 
@@ -32,34 +33,42 @@ buffers:
 	.res NUM_BUFS * 512, 0
 fnbuffer:
 	.res 512, 0
-databuffer:
-	.res 512, 0
 
 ; random accounting data
 fn_base:
 	.byte 0
-curbufptr:
+cur_buffer_ptr:
 	.byte 0
-bufferptr:
-	.byte 0
-databufferlen:
+cur_buffer_len:
 	.byte 0
 save_x:
 	.byte 0
 save_y:
 	.byte 0
-second_addr:
+channel:
 	.byte 0
 num_blocks:
 	.word 0
+fnlen:
+	.byte 0
+bufferno:
+	.byte 0
 
 buffer_alloc_map:
 	; $00 = free, $ff = allocated
 	.res NUM_BUFS, 0
 
+; number of valid data bytes in each buffer
+buffer_len:
+	.res NUM_BUFS + 1, 0
+
 ; current r/w pointer within the buffer
 buffer_ptr:
-	.res NUM_BUFS, 0
+	.res NUM_BUFS + 1, 0
+
+buffer_for_channel:
+	.res 16, 0
+
 
 
 ; XXX: initialize all this
@@ -81,13 +90,7 @@ buffer_ptr:
 ;****************************************
 cbdos_listn:
 	; ignore
-
-	stx save_x
-	sty save_y
-	jsr sdcard_init
-	ldy save_y
-	ldx save_x
-	rts ; do nothing
+	rts
 
 ;****************************************
 ; SECOND (after LISTEN)
@@ -97,9 +100,8 @@ cbdos_secnd: ; after listen
 	; will be a filename to be associated with channel x.
 	; Otherwise, we need to receive into channel x.
 
-	sta second_addr
+	sta channel ; we need it on UNLISTEN
 
-	pha
 	and #$f0
 	cmp #$f0
 	beq secnd_open
@@ -107,23 +109,19 @@ cbdos_secnd: ; after listen
 	cmp #$e0
 	beq secnd_close
 
-	; XXX TODO write to channel
-unimpl1:
-	brk
+; write to channel
+	ldx channel
+	lda buffer_for_channel,x
+	jmp switch_to_buffer
 
 secnd_open:
-	pla
 	lda #0
-	sta curbufptr
-	lda #<fnbuffer
-	sta curbuf
-	lda #>fnbuffer
-	sta curbuf + 1
-	rts
+	sta buffer_ptr + FN_BUFNO
+	lda #FN_BUFNO
+	jmp switch_to_buffer
 
 secnd_close:
 	; XXX TODO
-	pla
 	rts
 
 ;****************************************
@@ -131,20 +129,18 @@ secnd_close:
 ;****************************************
 cbdos_ciout:
 	sty save_y
-	ldy curbufptr
-	sta (curbuf),y
+	ldy cur_buffer_ptr
+	sta (buffer),y
 	ldy save_y
-	inc curbufptr
+	inc cur_buffer_ptr
+	; XXX overflow
 	rts
 
 ;****************************************
 ; UNLISTEN
 ;****************************************
 cbdos_unlsn:
-	stx save_x
-	sty save_y
-
-	lda second_addr
+	lda channel
 	and #$f0
 	cmp #$f0
 	beq unlsn_open
@@ -153,23 +149,33 @@ cbdos_unlsn:
 	rts
 
 unlsn_open:
+	stx save_x
+	sty save_y
+
+	lda cur_buffer_ptr
+	sta fnlen
+
+	lda channel
+	and #$0f
 	jsr buf_alloc
-	bmi no_bufs
+	bcs no_bufs
+
+	jsr sdcard_init
 
 	lda fnbuffer
 	cmp #'$'
-	bne not_dir
+	bne @not_dir
 
 ; DIR
 	jsr read_dir
-	jmp cbdos_unlsn2
+	jmp @unlisten_end
 
 ; READ FILE
-not_dir:
+@not_dir:
 	jsr read_file
-cbdos_unlsn2:
-	lda #0
-	sta bufferptr
+@unlisten_end:
+	jsr finished_with_buffer
+
 	ldy save_y
 	ldx save_x
 	rts
@@ -189,28 +195,31 @@ cbdos_talk:
 ; SECOND (after TALK)
 ;****************************************
 cbdos_tksa: ; after talk
-	rts
+	and #$0f ; XXX necessary?
+	tax
+	lda buffer_for_channel,x
+	jmp switch_to_buffer
 
 ;****************************************
 ; RECEIVE
 ;****************************************
 cbdos_acptr:
-	lda databufferlen
+	ldx bufferno
+	lda buffer_len,x
 	bne :+
 	lda #$02 ; file not found
 	sta $90
 	lda #0
 	rts
 
-:
-	stx save_x
-	ldx bufferptr
-	lda databuffer,x
-	ldx save_x
-	inc bufferptr
+:	stx save_y
+	ldy cur_buffer_ptr
+	lda (buffer),y
+	ldy save_y
+	inc cur_buffer_ptr
 	pha
-	lda bufferptr
-	cmp databufferlen
+	lda cur_buffer_ptr
+	cmp cur_buffer_len
 	bne :+
 acptr_fnf:
 	lda #$40 ; EOI
@@ -222,28 +231,47 @@ cbdos_untlk:
 	rts
 
 ;****************************************
-; allocate a buffer an set it as the
-; current one
-;   out: X: buffer#
-;        X==$FF, N=1 -> none
+; allocate a buffer for channel
+; * remember it as the channel's buffer
+; * switch to it
+;   in:  A: channel
+;   out: C: 0: success; 1: error
 buf_alloc:
+	tay ;  save channel
+; search & allocate free buffer
 	ldx #0
 :	lda buffer_alloc_map,x
 	beq :+
 	inx
 	cpx #NUM_BUFS
 	bne :-
-	ldx #$ff
+	sec ; error
 	rts
 :	dec buffer_alloc_map,x
-	lda #0
-	sta curbuf
+; associate channel with buffer
 	txa
+	sta buffer_for_channel,y
+; set buffer pointer to 0
+	tay
+	lda #0
+	sta buffer_ptr,y
+	tya
+switch_to_buffer:
+	sta bufferno
+; fetch buffer pointer
+	tay
+	pha
+	lda buffer_ptr,y
+	sta cur_buffer_ptr
+	pla
+; set zp word
 	asl
 	clc
 	adc #>buffers
-	sta curbuf + 1
-	txa ; set flags again
+	sta buffer + 1
+	lda #0
+	sta buffer
+	clc ; success
 	rts
 
 ;****************************************
@@ -255,30 +283,43 @@ buf_free:
 	rts
 
 ;****************************************
+; write back buffer prt
+finished_with_buffer:
+	ldx bufferno
+	lda cur_buffer_ptr
+	sta buffer_ptr,x
+	lda cur_buffer_len
+	sta buffer_len,x
+	rts
+
+;****************************************
 read_file:
 	jsr fat_mount
 
-	lda #0 ; zero-terminate
-	ldy curbufptr
-	sta (curbuf),y
-	lda curbuf
-	ldx curbuf + 1
+	lda #0 ; zero-terminate filename
+	ldy fnlen
+	sta fnbuffer,y
+	lda #<fnbuffer
+	ldx #>fnbuffer
 	ldy #O_RDONLY
 	jsr fat_open
 X1:
 	bne read_file_error
 
-	SetVector databuffer, read_blkptr
+	lda buffer
+	sta read_blkptr
+	lda buffer + 1
+	sta read_blkptr + 1
 
 	ldy #5
 	jsr fat_fread
 	lda #$ff
-	sta databufferlen ; XXX
+	sta cur_buffer_len ; XXX
 	rts
 
 read_file_error:
 	lda #0
-	sta databufferlen ; = file not found
+	sta cur_buffer_len ; = file not found
 	rts
 
 
@@ -286,10 +327,6 @@ read_file_error:
 read_dir:
 	jsr fat_mount
 
-	lda #<databuffer
-	sta curbuf
-	lda #>databuffer
-	sta curbuf+1
 	ldy #0
 	lda #<DIRSTART
 	jsr storedir
@@ -408,9 +445,9 @@ gt_1000:
 	lda #$22
 	jsr storedir
 
-	lda curbuf
+	lda buffer
 	sta krn_ptr3
-	lda curbuf + 1
+	lda buffer + 1
 	sta krn_ptr3 + 1
 	sty krn_tmp3
 	sty fn_base
@@ -468,7 +505,7 @@ end_of_dir:
 	jsr storedir
 	jsr storedir
 
-	sty databufferlen
+	sty cur_buffer_len
 
 	rts
 
@@ -477,10 +514,10 @@ txt_blocksfree:
 txt_blocksfree_end:
 
 storedir:
-	sta (curbuf),y
+	sta (buffer),y
 	iny
 	bne :+
-	inc curbuf + 1
+	inc buffer + 1
 :	rts
 
 allfiles:
