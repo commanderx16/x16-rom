b8c3d7e928131c7d166a9c4372ea27a21d847cb1
Michael Steil <mist64@mac.com>
2019-09-01 09:19:33 +0200

    [BASIC] DOS command; added directory code from the C128 monitor (monitor6.src)

--- basic/x16additions.s
+++ basic/x16additions.s
@@ -51,6 +51,10 @@ udtim  = $ffea
 screen = $ffed
 ;plot   = $fff0
 iobase = $fff3
+st = $90
+la = $b8
+sa = $b9
+fa = $ba
 
 monitor	jmp $ff00
 
@@ -78,47 +82,45 @@ vpoke	jsr getbyt ; bank
 	stx veradat
 	rts
 
-.if 0
 dos	cmp #'"'
 	beq dos3 ; dos with a command
-dos1	jsr listen_6f_or_error
+; no argument: print status
+dos1	jsr listen_cmd
 	jsr unlstn
-	jsr command_channel_talk
+	jsr getdev
+	jsr talk
+	lda #$6f
+	jsr tksa
 dos11	jsr iecin
 	jsr bsout
 	cmp #13
 	bne dos11
 	jmp untalk
 
-dos2	and #$0f
-	sta $ba ; fa
-	bne dos3
-	jmp snerr
-
+; string argument
 dos3	jsr chrget
 	beq dos1
-	cmp #$24
-	bne dos4
-
-	brk;XXX
-;XXX	jmp l8b79
-
-dos4	cmp #'8'
+	cmp #'$'
+	beq disk_dir
+; switch default drive
+	cmp #'8'
 	beq dos2
 	cmp #'9'
-	beq dos2
-	jsr listen_6f_or_error
+	bne dos2b
+dos2	and #$0f
+	sta fa
+	bne dos3
+; command
+dos2b	jsr listen_cmd
 	ldy #0
-	lda (txtptr),y
-	ldy #0
-dos5	jsr dos9
+dos9	lda (txtptr),y
+	beq dos7
+	cmp #'"'
 	beq dos6
 	jsr iecout
 	iny
-	bne dos5
-dos6	cmp #'"'
-	bne dos7
-	iny
+	bne dos9 ; "always"
+dos6	iny
 dos7	tya
 	clc
 	adc txtptr
@@ -127,16 +129,90 @@ dos7	tya
 	inc txtptr + 1
 dos8	jmp unlstn
 
-dos9	lda (txtptr),y
-	beq dosa
-	cmp #'"'
-dosa	rts
 
-command_channel_talk:
-	lda #$6f
-	pha
-	jsr set_drive
-	jsr talk
-	pla
-	jmp tksa
+listen_cmd:
+	jsr getdev
+	jsr listen
+	lda #$6F
+	jsr second
+	lda st
+	bmi device_not_present
+	rts
+
+device_not_present:
+	ldx #5 ; "DEVICE NOT PRESENT"
+	jmp error
+
+getdev:
+	lda #8
+	cmp fa
+	bcs :+
+	lda fa
+:	rts
+
+
+;  read & display the disk directory
+
+disk_dir
+	brk
+.if 0
+	ldy #$ff	;determine directory string length
+	ldx txtptr
+	dex
+
+10$	iny
+	inx
+	lda buf,x	;get a character
+	bne 10$		;...loop until eol
+
+	tya		;length
+	ldx txtptr	;fnadr low
+	ldy #>buf	;fnadr high
+	jsr setnam
+	lda #0		;la
+	ldx t0		;fa
+	ldy #$60	;sa
+	jsr setlfs
+	jsr open	;open directory channel
+	bcs disk_done	;...branch on error
+	ldx #0
+	jsr chkin	;make it an input channel
+
+	jsr crlf
+
+	ldy #3		;first pass only- trash first two bytes read
+
+20$	sty t1		;loop counter
+25$	jsr basin
+	sta t0		;get # blocks low
+	lda status
+	bne disk_done	;...branch if error
+	jsr basin
+	sta t0+1	;get # blocks high
+	lda status
+	bne disk_done	;...branch if error
+	dec t1
+	bne 25$		;...loop until done
+
+	jsr bindec	;convert # blocks to decimal
+	lda #0		;no leading zeros
+	ldx #8		;max digits
+	ldy #3		;required # shifts for decimal value
+	jsr unpack	;print # blocks
+	lda #' '
+	jsr bsout	;print space  (to match loaded directory display)
+
+30$	jsr basin	;read & print filename & filetype
+	beq 40$		;...branch if eol
+	ldx status
+	bne disk_done	;...branch if error
+	jsr bsout
+	bcc 30$		;...loop always
+
+40$	jsr crlf	;start a new line
+	jsr stop
+	beq disk_done	;...branch if user hit STOP
+	ldy #2
+	bne 20$		;...loop always
+
 .endif
