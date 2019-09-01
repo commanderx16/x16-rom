31f82ab9c254112c77e9ed3948952bee9a8b355c
Michael Steil <mist64@mac.com>
2019-09-01 10:54:17 +0200

    [BASIC] clean DOS/diirectory implementation

--- basic/x16additions.s
+++ basic/x16additions.s
@@ -52,12 +52,15 @@ screen = $ffed
 ;plot   = $fff0
 iobase = $fff3
 st = $90
+fnlen = $b7
 la = $b8
 sa = $b9
 fa = $ba
 
+;***************
 monitor	jmp $ff00
 
+;***************
 vpeek	jsr chrget
 	jsr chkopn ; open paren
 	jsr getbyt ; byte: bank
@@ -71,6 +74,7 @@ vpeek	jsr chrget
 	ldy veradat
 	jmp sngflt
 
+;***************
 vpoke	jsr getbyt ; bank
 	stx verahi
 	jsr chkcom
@@ -82,10 +86,53 @@ vpoke	jsr getbyt ; bank
 	stx veradat
 	rts
 
-dos	cmp #'"'
-	beq dos3 ; dos with a command
-; no argument: print status
-dos1	jsr listen_cmd
+;***************
+dos	beq ptstat      ;no argument: print status
+	jsr frmevl
+	jsr frestr      ;length in .a
+	cmp #0
+	beq ptstat      ;no argument: print status
+	ldx index1
+	ldy index1+1
+	jsr setnam
+	ldy #0
+	lda (index1),y
+; dir?
+	cmp #'$'
+	beq disk_dir
+; switch default drive?
+	cmp #'8'
+	beq dossw
+	cmp #'9'
+	beq dossw
+
+;***************
+; DOS command
+	jsr listen_cmd
+	ldy #0
+dos9	lda (index1),y
+	jsr iecout
+	iny
+	cpy fnlen
+	bne dos9
+	jmp unlstn
+
+listen_cmd:
+	jsr getdev
+	jsr listen
+	lda #$6f
+	jsr second
+	lda st
+	bmi :+
+	rts
+device_not_present:
+	ldx #5 ; "DEVICE NOT PRESENT"
+	jmp error
+
+
+;***************
+; print status
+ptstat	jsr listen_cmd
 	jsr unlstn
 	jsr getdev
 	jsr talk
@@ -97,51 +144,11 @@ dos11	jsr iecin
 	bne dos11
 	jmp untalk
 
-; string argument
-dos3	jsr chrget
-	beq dos1
-	cmp #'$'
-	beq disk_dir
+;***************
 ; switch default drive
-	cmp #'8'
-	beq dos2
-	cmp #'9'
-	bne dos2b
-dos2	and #$0f
+dossw	and #$0f
 	sta fa
-	bne dos3
-; command
-dos2b	jsr listen_cmd
-	ldy #0
-dos9	lda (txtptr),y
-	beq dos7
-	cmp #'"'
-	beq dos6
-	jsr iecout
-	iny
-	bne dos9 ; "always"
-dos6	iny
-dos7	tya
-	clc
-	adc txtptr
-	sta txtptr
-	bcc dos8
-	inc txtptr + 1
-dos8	jmp unlstn
-
-
-listen_cmd:
-	jsr getdev
-	jsr listen
-	lda #$6F
-	jsr second
-	lda st
-	bmi device_not_present
 	rts
-
-device_not_present:
-	ldx #5 ; "DEVICE NOT PRESENT"
-	jmp error
 
 getdev:
 	lda #8
@@ -151,68 +158,59 @@ getdev:
 :	rts
 
 
+;***************
 ;  read & display the disk directory
 
+LOGADD = 15
+
 disk_dir
-	brk
-.if 0
-	ldy #$ff	;determine directory string length
-	ldx txtptr
-	dex
-
-10$	iny
-	inx
-	lda buf,x	;get a character
-	bne 10$		;...loop until eol
-
-	tya		;length
-	ldx txtptr	;fnadr low
-	ldy #>buf	;fnadr high
-	jsr setnam
-	lda #0		;la
-	ldx t0		;fa
-	ldy #$60	;sa
+	lda #LOGADD     ;la
+	ldx #8          ;fa
+	ldy #$60        ;sa
 	jsr setlfs
-	jsr open	;open directory channel
-	bcs disk_done	;...branch on error
-	ldx #0
-	jsr chkin	;make it an input channel
+	jsr open        ;open directory channel
+	bcs disk_done   ;...branch on error
+	ldx #LOGADD
+	jsr chkin       ;make it an input channel
 
-	jsr crlf
+	jsr crdo
 
-	ldy #3		;first pass only- trash first two bytes read
+	ldy #4          ;first pass only- trash first four bytes read
 
-20$	sty t1		;loop counter
-25$	jsr basin
-	sta t0		;get # blocks low
-	lda status
-	bne disk_done	;...branch if error
-	jsr basin
-	sta t0+1	;get # blocks high
-	lda status
-	bne disk_done	;...branch if error
-	dec t1
-	bne 25$		;...loop until done
+@d20
+@d25	jsr basin
+	lda st
+	bne disk_done   ;...branch if error
+	dey
+	bne @d25        ;...loop until done
 
-	jsr bindec	;convert # blocks to decimal
-	lda #0		;no leading zeros
-	ldx #8		;max digits
-	ldy #3		;required # shifts for decimal value
-	jsr unpack	;print # blocks
+	jsr basin       ;get # blocks low
+	ldy st
+	bne disk_done   ;...branch if error
+	tax
+	jsr basin       ;get # blocks high
+	ldy st
+	bne disk_done   ;...branch if error
+	jsr linprt      ;print # blocks
+
 	lda #' '
-	jsr bsout	;print space  (to match loaded directory display)
+	jsr bsout       ;print space  (to match loaded directory display)
 
-30$	jsr basin	;read & print filename & filetype
-	beq 40$		;...branch if eol
-	ldx status
-	bne disk_done	;...branch if error
+@d30	jsr basin       ;read & print filename & filetype
+	beq @d40        ;...branch if eol
+	ldx st
+	bne disk_done   ;...branch if error
 	jsr bsout
-	bcc 30$		;...loop always
+	bcc @d30        ;...loop always
 
-40$	jsr crlf	;start a new line
+@d40	jsr crdo        ;start a new line
 	jsr stop
-	beq disk_done	;...branch if user hit STOP
+	beq disk_done   ;...branch if user hit STOP
 	ldy #2
-	bne 20$		;...loop always
+	bne @d20        ;...loop always
 
-.endif
+disk_done
+	jsr clrch
+	lda #LOGADD
+	sec
+	jmp close
