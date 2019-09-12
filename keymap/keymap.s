cbed27cc55cd077900121e58ece5471ebd5180be
Michael Steil <mist64@mac.com>
2019-09-12 10:32:40 +0200

    wip

---
+++ keymap/keymap.s
@@ -0,0 +1,19 @@
+
+.include "asm/409.s" ; US
+.include "asm/809.s" ; United Kingdom
+.include "asm/407.s" ; German
+.include "asm/40C.s" ; French
+.include "asm/410.s" ; Italian
+.include "asm/41D.s" ; Swedish
+.include "asm/416.s" ; Portuguese (Brazil ABNT)
+.include "asm/415.s" ; Polish (Programmers)
+.include "asm/40E.s" ; Hungarian
+.include "asm/406.s" ; Danish
+.include "asm/40A.s" ; Spanish
+.include "asm/40B.s" ; Finnish
+.include "asm/414.s" ; Norwegian
+.include "asm/807.s" ; Swiss German
+.include "asm/80C.s" ; Belgian French
+
+.segment "KBDMETA"
+	.byte 0 ; terminator
