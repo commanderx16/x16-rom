5f6edd67434c6ffce5d1e3ca45a01864fa4acacf
Michael Steil <mist64@mac.com>
2019-09-12 15:39:52 +0200

    [keymap] combined Finnish into Nordic

--- keymap/keymap.s
+++ keymap/keymap.s
@@ -1,17 +1,16 @@
 
-.include "asm/409.s" ; US
-.include "asm/809.s" ; United Kingdom
-.include "asm/407.s" ; German
-.include "asm/406.s" ; Danish [same as 41D Swedish/414 Norwegian -> "Nordic"]
-.include "asm/410.s" ; Italian
-.include "asm/415.s" ; Polish (Programmers)
-.include "asm/40E.s" ; Hungarian
-.include "asm/40A.s" ; Spanish
-.include "asm/40C.s" ; French
-.include "asm/807.s" ; Swiss German
-.include "asm/80C.s" ; Belgian French
-.include "asm/40B.s" ; Finnish
-.include "asm/416.s" ; Portuguese (Brazil ABNT)
+.include "asm/409.s"    ; US
+.include "asm/809.s"    ; United Kingdom
+.include "asm/407.s"    ; German
+.include "asm/nordic.s" ; Nordic [Danish, Swedish, Norwegian, Finnish]
+.include "asm/410.s"    ; Italian
+.include "asm/415.s"    ; Polish (Programmers)
+.include "asm/40E.s"    ; Hungarian
+.include "asm/40A.s"    ; Spanish
+.include "asm/40C.s"    ; French
+.include "asm/807.s"    ; Swiss German
+.include "asm/80C.s"    ; Belgian French
+.include "asm/416.s"    ; Portuguese (Brazil ABNT)
 
 
 .segment "KBDMETA"
--- keymap/make_keytab_asm.sh
+++ keymap/make_keytab_asm.sh
@@ -1,7 +1,5 @@
 layouts="406 407 409 40A 40B 40C 40E 410 414 415 416 41D 807 809 80C"
 
-rm -rf asm
-mkdir asm
 for layout in $layouts; do
 	filename=$(ls klc/${layout}\ *.klc)
 	python3 klc_to_asm.py "$filename" > asm/$layout.s
