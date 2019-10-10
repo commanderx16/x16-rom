7798fc5a81e01eb3370b88a775e6e68301885a7f
Michael Steil <mist64@mac.com>
2019-10-09 17:02:44 -0700

    line fixes

--- geos/kernal/graph/line.s
+++ geos/kernal/graph/line.s
@@ -28,27 +28,6 @@ bitmap_base = 0
 
 .segment "graph2a"
 
-PrepareXCoord:
-	ldx r11L
-	jsr _GetScanLine
-	lda r4L
-	and #%00000111
-	tax
-	lda BitMaskLeadingClear,x
-	sta r8H
-	lda r3L
-	and #%00000111
-	tax
-	lda BitMaskLeadingSet,x
-	sta r8L
-	lda r3L
-	and #%11111000
-	sta r3L
-	lda r4L
-	and #%11111000
-	sta r4L
-	rts
-
 ;---------------------------------------------------------------
 ; HorizontalLine                                          $C118
 ;
@@ -62,14 +41,7 @@ PrepareXCoord:
 _HorizontalLine:
 	jsr convcol
 	pha
-	ldx r11L
-	jsr _GetScanLineVera
-	AddW r3, r5
-
-	MoveW r4, r6
-	SubW r3, r6
-	IncW r6
-
+	jsr GetLineStart
 	pla
 	bbrf 7, dispBufferOn, @1 ; ST_WR_FORE
 	ldy r5L
@@ -78,13 +50,17 @@ _HorizontalLine:
 	sty veramid
 	ldy #$10
 	sty verahi
+	ldx r6H
+	phx
 	jsr HLine1
+	plx
+	stx r6H
 @1:	bbrf 6, dispBufferOn, @2 ; ST_WR_BACK
 	ldy r5L
 	sty veralo
 	ldy r5H
 	sty veramid
-	ldy #$10
+	ldy #$11
 	sty verahi
 	jmp HLine1
 @2:	rts
@@ -118,14 +94,7 @@ HLine1:
 ; Destroyed: a, x, y, r5 - r8
 ;---------------------------------------------------------------
 _InvertLine:
-	ldx r11L
-	jsr _GetScanLineVera
-	AddW r3, r5
-
-	MoveW r4, r6
-	SubW r3, r6
-	IncW r6
-
+	jsr GetLineStart
 	bbrf 7, dispBufferOn, @1 ; ST_WR_FORE
 	ldy #$10
 	PushW r6
@@ -175,22 +144,10 @@ ILine1:
 
 
 ImprintLine:
-	PushW r3
-	PushW r4
-	PushB dispBufferOn
-	ora #ST_WR_FORE | ST_WR_BACK
-	sta dispBufferOn
-	jsr PrepareXCoord
-	PopB dispBufferOn
-	lda r5L
-	ldy r6L
-	sta r6L
-	sty r5L
-	lda r5H
-	ldy r6H
-	sta r6H
-	sty r5H
-	bra RLin0
+	jsr GetLineStart
+	ldx #$10
+	ldy #$11
+	jmp RLine1
 
 ;---------------------------------------------------------------
 ; RecoverLine                                             $C11E
@@ -203,66 +160,43 @@ ImprintLine:
 ; Destroyed: a, x, y, r5 - r8
 ;---------------------------------------------------------------
 _RecoverLine:
-	PushW r3
-	PushW r4
-	PushB dispBufferOn
-	ora #ST_WR_FORE | ST_WR_BACK
-	sta dispBufferOn
-	jsr PrepareXCoord
-RLin0a:
-	PopB dispBufferOn
-RLin0:
-	ldy r3L
-	lda r3H
-	beq @1
-	inc r5H
-	inc r6H
-@1:
-	CmpW r3, r4
+	jsr GetLineStart
+	ldx #$11
+	ldy #$10
+RLine1:
+	lda #1
+	sta veractl
+	lda r5L
+	sta veralo
+	lda r5H
+	sta veramid
+	sty verahi
+	lda #0
+	sta veractl
+	lda r5L
+	sta veralo
+	lda r5H
+	sta veramid
+	stx verahi
+
+	ldy r6H
+	beq @2
+	ldy #0
+@1:	lda veradat
+	sta veradat2
+	dey
+	bne @1
+	dec r6H
+	bne @1
+@2:	ldy r6L
 	beq @4
-	SubW r3, r4
-	lsr r4H
-	ror r4L
-	lsr r4L
-	lsr r4L
-	lda r8L
-	jsr RecLineHelp
-@2:	tya
-	addv 8
-	tay
-	bcc @3
-	inc r5H
-	inc r6H
-@3:	dec r4L
-	beq @5
-	lda (r6),Y
-	sta (r5),Y
-	bra @2
-@4:	lda r8L
-	ora r8H
-	bra @6
-@5:	lda r8H
-@6:	jsr RecLineHelp
-	jmp HLinEnd2
-
-RecLineHelp:
-	sta r7L
-	and (r5),Y
-	sta r7H
-	lda r7L
-	eor #$FF
-	and (r6),Y
-	ora r7H
-	sta (r5),Y
-	rts
-
-HLinEnd1:
-	sta (r6),Y
-	sta (r5),Y
-HLinEnd2:
-	PopW r4
-	PopW r3
-	rts
+	dey
+@3:	lda veradat
+	sta veradat2
+	dey
+	cpy #$ff
+	bne @3
+@4:	rts
 
 ;---------------------------------------------------------------
 ; VerticalLine                                            $C121
@@ -294,6 +228,7 @@ _VerticalLine:
 	ldy #0
 	jsr VLine1
 	plx
+	tya
 @1:	bbrf 6, dispBufferOn, VLine1 ; ST_WR_BACK
 	ldy #1
 	jmp VLine1
@@ -348,3 +283,13 @@ convcol:
 @3:	lda #16+15
 	rts
 .endif
+
+
+GetLineStart:
+	ldx r11L
+	jsr _GetScanLineVera
+	AddW r3, r5
+	MoveW r4, r6
+	SubW r3, r6
+	IncW r6
+	rts
