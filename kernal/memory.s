a20035c6259a8892bb1b15d71a0461806110ffa6
Michael Steil <mist64@mac.com>
2019-12-23 11:16:05 +0100

    WIP

--- kernal/memory.s
+++ kernal/memory.s
@@ -21,6 +21,8 @@
 ;            r1   number of bytes
 ;---------------------------------------------------------------
 memory_fill:
+	ldx r0H
+	phx
 	ldx r1H
 	beq @2
 	ldy #0
@@ -36,7 +38,9 @@ memory_fill:
 	sta (r0),y
 	cpy #0
 	bne @3
-@4:	rts
+@4:	plx
+	stx r0H
+	rts
 
 ;---------------------------------------------------------------
 ; memory_copy
@@ -106,4 +110,73 @@ memory_copy:
 	dex
 	bra @A
 
+;---------------------------------------------------------------
+; memory_crc
+;
+; Function:  Calculate the CRC16 of a memory region.
+;
+; Pass:      r0   address
+;            r1   number of bytes
+;
+; Return:    r2   CRC16 result
+;---------------------------------------------------------------
 memory_crc:
+	lda #$ff
+	sta r2L
+	sta r2H
+
+	PushB r0H
+	lda r1H
+	beq @2
+	pha
+	ldy #0
+@1:	lda (r0),y
+	jsr crc16_f
+	iny
+	bne @1
+	inc r0H
+	dec r1H
+	bne @1
+	PopB r1H
+@2:	ldy r1L
+	beq @4
+@3:	dey
+	lda (r0),y
+	jsr crc16_f
+	cpy #0
+	bne @3
+@4:	PopB r0H
+	rts
+
+; This is taken from
+; http://www.6502.org/source/integers/crc-more.html
+; (November 23rd, 2004, "alternate ending" version, preserving .Y)
+crc16_f:
+	eor r2H         ; A contained the data
+	sta r2H         ; XOR it into high byte
+	lsr             ; right shift A 4 bits
+	lsr             ; to make top of x^12 term
+	lsr             ; ($1...)
+	lsr
+	tax             ; save it
+	asl             ; then make top of x^5 term
+	eor r2L         ; and XOR that with low byte
+	sta r2L         ; and save
+	txa             ; restore partial term
+	eor r2H         ; and update high byte
+	sta r2H         ; and save
+	asl             ; left shift three
+	asl             ; the rest of the terms
+	asl             ; have feedback from x^12
+	tax             ; save bottom of x^12
+	asl             ; left shift two more
+	asl             ; watch the carry flag
+	eor r2H         ; bottom of x^5 ($..2.)
+	sta r2H         ; save high byte
+	txa             ; fetch temp value
+	rol             ; bottom of x^12, middle of x^5!
+	eor r2L         ; finally update low byte
+	ldx r2H         ; then swap high and low bytes
+	sta r2H
+	stx r2L
+	rts
