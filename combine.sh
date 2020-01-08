a042b024f57407ac5d9b03cb7d0eb7fa18cc4f1a
Michael Steil <mist64@mac.com>
2020-01-08 13:46:50 +0100

    WIP

--- combine.sh
+++ combine.sh
@@ -1,13 +1,15 @@
-# header  & basic
-dd if=/Users/mist/Library/Mobile\ Documents/com\~apple\~CloudDocs/Applications/xscpu64.app/Contents/Resources/ROM/SCPU64/scpu64 of=scpu64 bs=256 count=33
+# header
+dd if=/Users/mist/Library/Mobile\ Documents/com\~apple\~CloudDocs/Applications/xscpu64.app/Contents/Resources/ROM/SCPU64/scpu64 of=scpu64 bs=256 count=1
+
+# basic #1
 
 # hi_basic #1
-dd if=/Users/mist/Library/Mobile\ Documents/com\~apple\~CloudDocs/Applications/xscpu64.app/Contents/Resources/ROM/SCPU64/scpu64 bs=256 skip=33 count=5 >> scpu64
+cat basic-orig.bin >> scpu64
 # kernal #1
 dd if=c64-rom.bin bs=256 skip=5 count=27 >> scpu64
 
 # hi_basic #2
-dd if=/Users/mist/Library/Mobile\ Documents/com\~apple\~CloudDocs/Applications/xscpu64.app/Contents/Resources/ROM/SCPU64/scpu64 bs=256 skip=33 count=5 >> scpu64
+dd if=basic-orig.bin bs=256 skip=32 >> scpu64
 # kernal #2
 dd if=c64-rom.bin bs=256 skip=5 count=27 >> scpu64
 
