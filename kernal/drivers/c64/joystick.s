a2260c7e6a477ffae689cab50b44cca0ec0ad940
Michael Steil <mist64@mac.com>
2020-01-09 22:50:40 +0100

    WIP

--- kernal/drivers/c64/joystick.s
+++ kernal/drivers/c64/joystick.s
@@ -44,7 +44,32 @@ joystick_scan:
 ;              on NES and SNES.
 ;---------------------------------------------------------------
 joystick_get: ; XXX
+	; C64 joystick button layout:
+	; | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+	; |   |   |   | A |RT |LT |DN |UP |
+	cpx #2
+	bcc :+
 	lda #0
 	ldx #0
 	ldy #$ff
 	rts
+
+:	txa
+	eor #1 ; $dc00 is port 2, $dc01 is port 1
+	tax
+	lda $dc00,x
+	eor #$ff
+	pha
+	and #$0f
+	tax
+	pla
+	cmp #$10      ; set .C if fire button down
+	lda convtab,x
+	ror           ; fire button to MSB
+	eor #$ff
+	ldx #$08      ; 1000: C64
+	ldy #$ff
+	rts
+
+convtab:
+	.byte $00,$10,$08,$18,$04,$14,$0c,$1c,$02,$12,$0a,$1a,$06,$16,$0e,$1e
