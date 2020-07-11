5e3bf08ea632fedcec4486c22e58dd8363cf3c17
Michael Steil <mist64@mac.com>
2020-07-11 16:53:23 +0200

    removed unused symbols, allocated zero page

--- cbdos/zeropage.s
+++ cbdos/zeropage.s
@@ -1,10 +1,10 @@
-.global krn_ptr1, krn_ptr2, krn_ptr3, filenameptr, dirptr, read_blkptr, write_blkptr, buffer, bank_save
+.global krn_ptr1, krn_ptr3, filenameptr, dirptr, read_blkptr, buffer, bank_save
+.global fat32_bufptr, fat32_ptr, fat32_ptr2
 
 .segment "ZPCBDOS" : zeropage
 
+; CBDOS
 krn_ptr1:
-	.res 2
-krn_ptr2:
 	.res 2
 krn_ptr3:
 	.res 2
@@ -14,9 +14,15 @@ dirptr:
 	.res 2
 read_blkptr:
 	.res 2
-write_blkptr:
-	.res 2
 buffer:
 	.res 2
 bank_save:
 	.res 1
+
+; FAT 32
+fat32_bufptr:
+	.res 2 ; word - Internally used by FAT32 code
+fat32_ptr:
+	.res 2 ; word - Buffer pointer to various functions
+fat32_ptr2:
+	.res 2 ; word - Buffer pointer to various functions
