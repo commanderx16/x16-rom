5c28bd3519ef0d3367fca5adb0e043bf9f577fcf
Michael Steil <mist64@mac.com>
2021-03-29 08:14:24 +0200

    adapted code to match coding style, no code changes

--- kernal/drivers/x16/i2c.s
+++ kernal/drivers/x16/i2c.s
@@ -22,144 +22,163 @@
 ; SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
-			.setcpu		"6502"
+.setcpu		"6502"
 
-			.export _i2cStart
-			.export _i2cStop
-			.export _i2cAck
-			.export _i2cNack
-			.export _i2cWrite
-			.export _i2cRead
+.export _i2cStart
+.export _i2cStop
+.export _i2cAck
+.export _i2cNack
+.export _i2cWrite
+.export _i2cRead
 
-			VIA1_BASE   = $8100
-			PRA  = VIA1_BASE+1
-			DDRA = VIA1_BASE+3
+VIA1_BASE   = $8100
+PRA  = VIA1_BASE+1
+DDRA = VIA1_BASE+3
 
-			SDA = (1 << 0)
-			SCL = (1 << 1)
+SDA = (1 << 0)
+SCL = (1 << 1)
 
-			.segment "CODE"
-
-
-_i2cWrite:	ldx #$00
-			stx PRA
-			ldx #$09
-@loop:		dex
-			beq @ack
-			rol
-			bcc @send_zero
-			pha
-			lda #$01
-			jsr send_bit
-			pla
-			jmp @loop
-@send_zero:	pha
-			lda #$00
-			jsr send_bit
-			pla
-			jmp @loop
-@ack:		; ldx #$00 		; x should be zero allready
-			jsr rec_bit		; ack in accu 0 = success
-			eor #$01		; return 1 on success, 0 on fail
-@end:		rts
+.segment "CODE"
 
 
-_i2cRead:	lda #$00
-			sta PRA
-			pha
-			ldx #$09
-@loop:		dex
-			beq @end
-			jsr rec_bit
-			ror
-			pla
-			rol
-			pha
-			jmp @loop
-@end:		; ldx #$00 x should be zero allready
-			pla
-			rts
+_i2cWrite:
+	ldx #0
+	stx PRA
+	ldx #9
+@loop:	dex
+	beq @ack
+	rol
+	bcc @send_zero
+	pha
+	lda #1
+	jsr send_bit
+	pla
+	jmp @loop
+@send_zero:
+	pha
+	lda #0
+	jsr send_bit
+	pla
+	jmp @loop
+@ack:
+	; ldx #0                ; x should be zero already
+	jsr rec_bit             ; ack in accu 0 = success
+	eor #1                  ; return 1 on success, 0 on fail
+@end:	rts
 
 
-send_bit:	cmp #$01		;bit in accu
-			beq @set_sda
-@clear_sda:	jsr sda_low
-			jmp @clock_out
-@set_sda:	jsr sda_high
-			jmp	@clock_out
-@clock_out:	jsr scl_high
-			jsr scl_low
-			jsr sda_low
-			rts
+_i2cRead:
+	lda #0
+	sta PRA
+	pha
+	ldx #9
+@loop:	dex
+	beq @end
+	jsr rec_bit
+	ror
+	pla
+	rol
+	pha
+	jmp @loop
+@end:
+	; ldx #0                ; x should be zero already
+	pla
+	rts
 
 
-rec_bit:	jsr sda_high
-			jsr scl_high
-			lda PRA
-			and #SDA
-			bne @is_one
-			lda #$00
-			jmp @end
-@is_one:	lda #$01
-@end:		jsr scl_low
-			jsr sda_low
-			rts
+send_bit:
+	cmp #1                  ; bit in accu
+	beq @set_sda
+@clear_sda:
+	jsr sda_low
+	jmp @clock_out
+@set_sda:
+	jsr sda_high
+	jmp	@clock_out
+@clock_out:
+	jsr scl_high
+	jsr scl_low
+	jsr sda_low
+	rts
 
 
-_i2cStart:	jsr sda_low
-			jsr scl_low
-			rts
+rec_bit:
+	jsr sda_high
+	jsr scl_high
+	lda PRA
+	and #SDA
+	bne @is_one
+	lda #0
+	jmp @end
+@is_one:
+	lda #1
+@end:	jsr scl_low
+	jsr sda_low
+	rts
 
 
-_i2cStop:	jsr scl_high
-			jsr sda_high
-			rts
+_i2cStart:
+	jsr sda_low
+	jsr scl_low
+	rts
 
 
-_i2cAck:	pha
-			lda #$00
-			jsr send_bit
-			pla
-			rts
+_i2cStop:
+	jsr scl_high
+	jsr sda_high
+	rts
 
 
-_i2cNack:	pha
-			lda #$01
-			jsr send_bit
-			pla
-			rts
+_i2cAck:
+	pha
+	lda #0
+	jsr send_bit
+	pla
+	rts
 
 
-sda_low:	pha
-			lda DDRA
-			ora #SDA
-			sta DDRA
-			pla
-			rts
+_i2cNack:
+	pha
+	lda #1
+	jsr send_bit
+	pla
+	rts
 
 
-sda_high:	pha
-			lda #SDA
-			eor #$FF
-			and DDRA
-			sta DDRA
-			pla
-			rts
+sda_low:
+	pha
+	lda DDRA
+	ora #SDA
+	sta DDRA
+	pla
+	rts
 
 
-scl_low:	pha
-			lda DDRA
-			ora #SCL
-			sta DDRA
-			pla
-			rts
+sda_high:
+	pha
+	lda #SDA
+	eor #$FF
+	and DDRA
+	sta DDRA
+	pla
+	rts
 
 
-scl_high:	pha
-			lda #SCL
-			eor #$FF
-			and DDRA
-			sta DDRA
-			pla
-			rts
+scl_low:
+	pha
+	lda DDRA
+	ora #SCL
+	sta DDRA
+	pla
+	rts
+
+
+scl_high:
+	pha
+	lda #SCL
+	eor #$FF
+	and DDRA
+	sta DDRA
+	pla
+	rts
 
