16f84b679c7508a81152064567653fe05c05d0af
Michael Steil <mist64@mac.com>
2021-04-02 07:01:29 +0200

    cache activity LED state

--- kernal/drivers/x16/smc.s
+++ kernal/drivers/x16/smc.s
@@ -3,10 +3,14 @@
 ;----------------------------------------------------------------------
 ; (C)2021 Michael Steil, License: 2-clause BSD
 
+.segment "KVAR"
+
+activity_led_state:
+	.res 0
+
 .segment "I2C"
 
 .import i2c_write_byte
-
 .export smc_set_activity_led
 
 uc_address = $42
@@ -19,6 +23,10 @@ uc_address = $42
 ; 0x05 0x00-0xFF - Activity LED Level (PWM)
 
 smc_set_activity_led:
+	cmp activity_led_state
+	beq @rts
+	sta activity_led_state
 	ldx #uc_address
 	ldy #5
 	jmp i2c_write_byte
+@rts:	rts
