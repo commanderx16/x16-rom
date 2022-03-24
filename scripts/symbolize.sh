74e797887fdb178cb77c7117788a616a4106bbdd
Michael Steil <mist64@mac.com>
2022-03-24 18:23:11 +0100

    [build] removed duplicates from rom_labels.h

--- scripts/symbolize.sh
+++ scripts/symbolize.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo "uint16_t addresses_bank$1[] = {"
-for i in $(cat $2 | grep "^.....[ABCDEF]" | cut -c 6-9); do
+for i in $(cat $2 | sort | uniq | grep "^.....[ABCDEF]" | cut -c 6-9); do
 	echo -n "0x$i, "
 done
 echo
 echo "};"
 
 echo "char *labels_bank$1[] = {"
-for i in $(cat $2 | grep "^.....[ABCDEF]" | cut -c 12-); do
+for i in $(cat $2 | sort | uniq | grep "^.....[ABCDEF]" | cut -c 12-); do
 	echo -n "\"$i\", "
 done
 echo
