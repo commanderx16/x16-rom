a6056f3cbf6a9599d1012bf7bb585067f444f352
Michael Steil <mist64@mac.com>
2022-04-12 07:51:29 +0200

    [KERNAL] have MACPTR call in LOAD go through vector
    This is needed for intercepting the call in the emulator.

--- kernal/cbm/channel/load.s
+++ kernal/cbm/channel/load.s
@@ -4,8 +4,6 @@
 ; (C)1983 Commodore Business Machines (CBM)
 ; additions: (C)2020 Michael Steil, License: 2-clause BSD
 
-.import macptr
-
 ;**********************************
 ;* load ram function              *
 ;*                                *
