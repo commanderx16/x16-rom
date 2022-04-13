ec1b5c2d2f8d3e0d6c40f0950843ed9e2e823d3a
Michael Steil <mist64@mac.com>
2022-04-13 22:36:25 +0200

    [KERNAL] NMI & BRK will enter monitor
    * NMI does not care about the STOP key any more. NMI on the X16 is
      a button on the X16 system, not a key on the keyboard, so it
      should not need to be combined with another key; also, the PS/2
      keyboard might be out of sync or something, which is what the
      user wants to fix in the first place.
    * Both NMI and BRK enter the monitor instead of BASIC. This is
      useful for asm programming. (You can always exit the monitor
      with 'X'.) Maybe this should even become the entry point to
      a freezer-like debugger at some point, which saves the VERA
      state and changes as little in RAM as possible.
    
    Closes #264

--- kernal/cbm/nmi.s
+++ kernal/cbm/nmi.s
@@ -6,31 +6,15 @@
 
 .feature labels_without_colons
 
-stop  = $ffe1
+monitor = $fecc
 .import enter_basic, cint, ioinit, restor, nminv
 
 .export nmi, nnmi, timb
 
 	.segment "NMI"
 
-nmi	sei             ;no irq's allowed...
-	jmp (nminv)     ;...could mess up cassettes
-nnmi	pha
-.ifp02
-	txa
-	pha
-	tya
-	pha
-	cld
-.else
-	phx
-	phy
-.endif
-;
-; check for stop key down
-;
-	jsr stop        ;no .y
-	bne prend       ;no stop key...
+nmi	jmp (nminv)
+nnmi
 ;
 ; timb - where system goes on a brk instruction
 ;
@@ -38,17 +22,5 @@ timb	jsr restor      ;restore system indirects
 	jsr ioinit      ;restore i/o for basic
 	jsr cint        ;restore screen for basic
 	clc
-	jmp enter_basic
+	jmp monitor
 
-prend
-.ifp02
-	pla
-	tay
-	pla
-	tax
-.else
-	ply
-	plx
-.endif
-	pla
-	rti
