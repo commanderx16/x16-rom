f84fb70e12e3e118b9ccf111731de814346f4dff
Michael Steil <mist64@mac.com>
2022-04-16 00:36:13 +0200

    wip

--- monitor/io.s
+++ monitor/io.s
@@ -1,3 +1,5 @@
+readst = $ffb7
+
 fnlen	:= $1111  ; length of current file name
 ;sa	:= $1111  ; secondary address
 fa	:= $1111  ; device number
@@ -5,6 +7,8 @@ fa	:= $1111  ; device number
 xmon2 = $1111 ; XXX
 loop4 = $1111 ; XXX
 insrt = $1111 ; XXX
+
+.feature labels_without_colons
 
 .include "kernal.i"
 
@@ -163,6 +167,8 @@ setnam2:
 	lda fnlen
 	jmp setnam
 
+LF0BD:	.byte "I/O ERROR"
+
 ; ----------------------------------------------------------------
 ; "@" - send drive command
 ;	without arguments, this reads the drive status
@@ -174,5 +180,170 @@ cmd_at:
 	cmp #'$'
 print_drive_status:
 	jmp print_cr_then_input_loop
-LF0BD:
-	.byte "I/O ERROR"
+
+
+; ----------------------------------------------------------------
+; ----------------------------------------------------------------
+; ----------------------------------------------------------------
+
+;***************
+dos	jsr basin_skip_spaces_cmp_cr
+	beq ptstat      ;no argument: print status
+	ldy #0
+	cmp #'"'
+	bne @err
+:	jsr basin_cmp_cr
+	beq @done
+	sta tmp16,y
+	iny
+	cpy #40
+	bne :-
+@err:	jmp syntax_error
+@done:	tya
+	sta verck       ;save length
+	ldx #<tmp16
+	ldy #>tmp16
+	jsr setnam
+	lda tmp16
+; dir?
+	cmp #'$'
+	beq disk_dir
+; switch default drive?
+	cmp #'8'
+	beq dossw
+	cmp #'9'
+	beq dossw
+
+;***************
+; DOS command
+	jsr listen_cmd
+	ldy #0
+:	lda tmp16,y
+	jsr iecout
+	iny
+	cpy verck       ;length?
+	bne :-
+	jmp unlstn
+
+listen_cmd:
+	jsr getfa
+	jsr listen
+	lda #$6f
+	jsr second
+	jsr readst
+	bmi device_not_present
+	rts
+device_not_present:
+	jmp LB3A6
+
+
+;***************
+; print status
+ptstat	jsr listen_cmd
+	jsr unlstn
+	jsr getfa
+	jsr talk
+	lda #$6f
+	jsr tksa
+dos11	jsr iecin
+	jsr bsout
+	cmp #13
+	bne dos11
+	jmp untalk
+
+;***************
+; switch default drive
+dossw	and #$0f
+	sta basic_fa
+	rts
+
+getfa:
+	lda #8
+	cmp basic_fa
+	bcs :+
+	lda basic_fa
+:	rts
+
+
+;***************
+;  read & display the disk directory
+
+LOGADD = 15
+
+disk_dir
+	jsr getfa
+	tax
+	lda #LOGADD     ;la
+	ldy #$60        ;sa
+	jsr setlfs
+	jsr open        ;open directory channel
+	jsr readst
+	bpl :+
+	lda #LOGADD
+	jsr close
+	jmp device_not_present
+:	ldx #LOGADD
+	jsr chkin       ;make it an input channel
+
+	jsr crdo
+
+	ldy #4          ;first pass only- trash first four bytes read
+
+@d20
+@d25	jsr basin
+	jsr readst
+	bne disk_done   ;...branch if error
+	dey
+	bne @d25        ;...loop until done
+
+	jsr basin       ;get # blocks low
+	pha
+	jsr readst
+	tay
+	pla
+	cpy #0
+	bne disk_done   ;...branch if error
+	tax
+	jsr basin       ;get # blocks high
+	pha
+	jsr readst
+	tay
+	pla
+	cpy #0
+	bne disk_done   ;...branch if error
+	jsr linprt      ;print # blocks
+
+	lda #' '
+	jsr bsout       ;print space  (to match loaded directory display)
+
+@d30	jsr basin       ;read & print filename & filetype
+	beq @d40        ;...branch if eol
+	pha
+	jsr readst
+	tax
+	pla
+	cpx #0
+	bne disk_done   ;...branch if error
+	jsr bsout
+	bcc @d30        ;...loop always
+
+@d40	jsr crdo        ;start a new line
+	jsr stop
+	beq disk_done   ;...branch if user hit STOP
+	ldy #2
+	bne @d20        ;...loop always
+
+disk_done
+	jsr clrch
+	lda #LOGADD
+	sec
+	jmp close
+
+crdo:	lda #13
+	jmp bsout
+
+linprt:
+	rts
+
+basic_fa = $1200
+verck = $1202
