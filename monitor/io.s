ceb8518e4422549e56c34ddfc9ee15571e3b5424
Michael Steil <mist64@mac.com>
2022-04-16 00:14:35 +0200

    wip

--- monitor/io.s
+++ monitor/io.s
@@ -1,5 +1,5 @@
 fnlen	:= $1111  ; length of current file name
-sa	:= $1111  ; secondary address
+;sa	:= $1111  ; secondary address
 fa	:= $1111  ; device number
 
 xmon2 = $1111 ; XXX
@@ -40,13 +40,6 @@ insrt = $1111 ; XXX
 .export cmd_ls
 
 .segment "monitor"
-
-listen_command_channel:
-	lda #$6F
-	jsr init_and_listen
-	lda status
-	bmi LB3A6
-	rts
 
 ; ----------------------------------------------------------------
 ; "L"/"S" - load/save file
@@ -176,169 +169,10 @@ setnam2:
 ;	$ shows the directory
 ; ----------------------------------------------------------------
 cmd_at:
-	jsr listen_command_channel
 	jsr basin_cmp_cr
 	beq print_drive_status
 	cmp #'$'
-	beq LB475
-LB458:	jsr iecout
-	jsr basin_cmp_cr
-	bne LB458
-	jsr unlstn
-	jmp print_cr_then_input_loop
-
-; just print drive status
 print_drive_status:
-	jsr print_cr
-	jsr unlstn
-	jsr talk_cmd_channel
-	jsr cat_line_iec
-	jmp input_loop
-
-; show directory
-LB475:	jsr unlstn
-	jsr print_cr
-	lda #$F0 ; sec address
-	jsr init_and_listen
-	lda #'$'
-	jsr iecout
-	jsr unlstn
-	jsr directory
-	jmp input_loop
-
-init_and_listen:
-	pha
-	jsr init_drive
-	jsr listen
-	pla
-	jmp second
-
-talk_cmd_channel:
-	lda #$6F
-init_and_talk:
-	pha
-	jsr init_drive
-	jsr talk
-	pla
-	jmp tksa
-
-cat_line_iec:
-	jsr iecin
-	jsr LE716 ; KERNAL: output character to screen
-	cmp #CR
-	bne cat_line_iec
-	jmp untalk
-
-directory:
-	lda #$60
-	sta sa
-	jsr init_and_talk
-	jsr iecin
-	jsr iecin ; skip load address
-LBCDF:	jsr iecin
-	jsr iecin ; skip link word
-	jsr iecin
-	tax
-	jsr iecin ; line number (=blocks)
-	ldy status
-	bne LBD2F ; error
-	jsr LBC4C ; print A/X decimal
-	lda #' '
-	jsr LE716 ; KERNAL: output character to screen
-	ldx #$18
-LBCFA:	jsr iecin
-LBCFD:	ldy status
-	bne LBD2F ; error
-	cmp #CR
-	beq LBD09 ; convert $0D to $1F
-	cmp #$8D
-	bne LBD0B ; also convert $8D to $1F
-LBD09:	lda #$1F ; ???BLUE
-LBD0B:	jsr LE716 ; KERNAL: output character to screen
-	inc insrt
-	jsr getin
-	cmp #KEY_STOP
-	beq LBD2F
-	cmp #' '
-	bne LBD20
-LBD1B:	jsr getin
-	beq LBD1B ; space pauses until the next key press
-LBD20:	dex
-	bpl LBCFA
-	jsr iecin
-	bne LBCFD
-	lda #CR
-	jsr LE716 ; KERNAL: output character to screen
-LBD2D:	bne LBCDF ; next line
-LBD2F:	jmp LF646 ; CLOSE
-
-init_drive:
-	lda #0
-	sta status ; clear status
-	lda #8
-	cmp fa ; drive 8 and above ok
-	bcc LBD3F
-LBD3C:	sta fa ; otherwise set drive 8
-LBD3E:	rts
-
-LBD3F:	lda #9
-	cmp fa
-	bcs LBD3E
-	lda #8
-LBD47:
-	bne LBD3C
-	lda zp3 ; XXX ???
-LBD4B:	ldy status
-	bne LBD7D
-	cmp #CR
-	beq LBD57
-	cmp #$8D
-	bne LBD59
-LBD57:	lda #$1F
-LBD59:	jsr LE716 ; KERNAL: output character to screen
-	inc insrt
-	jsr getin
-	cmp #KEY_STOP
-	beq LBD7D
-	cmp #$20
-	bne LBD6E
-LBD69:	jsr getin
-	beq LBD69
-LBD6E:	dex
-	bpl LBD47 + 1 ; ??? XXX
-	jsr iecin
-	bne LBD4B
-	lda #CR
-	jsr LE716 ; KERNAL: output character to screen
-	bne LBD2D
-LBD7D:	jmp LF646 ; CLOSE
-
-	lda #0
-	sta status
-	lda #8
-	cmp fa
-	bcc LBD8D
-LBD8A:	sta fa
-LBD8C:	rts
-
-LBD8D:	lda #9
-	cmp fa
-	bcs LBD8C
-	lda #8
-	bne LBD8A ; always
-
-
-LF646:
-	jsr mjsrfar
-	.word xmon2 ; IEC close
-	.byte BANK_KERNAL
-	rts
-
-LE716:
-	jsr mjsrfar
-	.word loop4 ; screen CHROUT
-	.byte BANK_KERNAL
-	rts
-
+	jmp print_cr_then_input_loop
 LF0BD:
 	.byte "I/O ERROR"
