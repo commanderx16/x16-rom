900f991830d009b43976cc7d6a6520561701dbae
Michael Steil <mist64@mac.com>
2022-05-01 13:09:17 +0200

    wip

--- keymap/keymap.s
+++ keymap/keymap.s
@@ -1,33 +1,17 @@
 .segment "KBDMETA"
 
 .include "asm/409.s"    ; US
-.include "asm/i409.s"
 .include "asm/809.s"    ; United Kingdom
-.include "asm/i809.s"
 .include "asm/407.s"    ; German
-.include "asm/i407.s"
 .include "asm/41D.s"    ; Swedish
-.include "asm/i41D.s"
 .include "asm/410.s"    ; Italian
-.include "asm/i410.s"
 .include "asm/415.s"    ; Polish (Programmers)
-.include "asm/i415.s"
 .include "asm/40E.s"    ; Hungarian
-.include "asm/i40E.s"
 .include "asm/40A.s"    ; Spanish
-.include "asm/i40A.s"
 .include "asm/40C.s"    ; French
-.include "asm/i40C.s"
 .include "asm/807.s"    ; Swiss German
-.include "asm/i807.s"
 .include "asm/80C.s"    ; Belgian French
-.include "asm/i80C.s"
 .include "asm/416.s"    ; Portuguese (Brazil ABNT)
-.include "asm/i416.s"
 
 .segment "KBDMETA"
 	.byte 0 ; terminator
-
-.segment "IKBDMETA"
-
-.segment "IKBDMETA"
