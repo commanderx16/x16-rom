cd78c4243725779ffdc72e36e6614763f4203956
stefan-b-jakobsson <stefan.b.jakobsson@gmail.com>
2022-10-15 15:32:40 +0300

    Implemented BANK statement

--- basic/x16additions.s
+++ basic/x16additions.s
@@ -606,6 +606,18 @@ ckeymap:
 @fcerr:	jmp fcerr
 
 ;***************
+.export curbank
+.segment "BVARS2"
+	curbank: .res 1
+
+.segment "BASIC"
+.export setbank
+setbank:
+	jsr getbyt
+	stx curbank
+	rts
+
+;***************
 test:
 	beq @test0
 	jsr getbyt
