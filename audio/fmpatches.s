01463b114e385e6db3f12634268361cf9acc41b0
ZeroByteOrg <thatguy@zerobyte.org>
2022-12-21 15:33:07 -0600

    additional instruments for playing around with during dev...

--- audio/fmpatches.s
+++ audio/fmpatches.s
@@ -9,18 +9,54 @@
 
 .segment "PATCHDATA"
 fm_patches:
-marimba:
+YMP_marimba:
 	.byte $DC,$00,$1B,$67,$61,$31,$21,$17,$1F,$0A,$DF,$5F,$DE
 	.byte $DE,$0E,$10,$09,$07,$00,$05,$07,$04,$FF,$A0,$16,$17
 
-finger_bass_1:
+YMP_finger_bass_1:
 	.byte	$F0,$00,$10,$10,$10,$10,$17,$17
 	.byte	$3F,$0A,$9E,$9C,$98,$9C,$0E,$04
 	.byte	$0A,$05,$08,$09,$09,$09,$B6,$C6
 	.byte	$C6,$C6
 
+YMP_Electric_Piano_1:
+	.byte	$D4,$00,$31,$6A,$21,$31,$17,$39
+	.byte	$14,$0A,$9A,$DA,$98,$D8,$0F,$07
+	.byte	$0C,$0C,$00,$03,$05,$05,$26,$46
+	.byte	$28,$28
 
-.define PATCHTABLE marimba, finger_bass_1, marimba
+YMP_Electric_Piano_2:
+	.byte	$D4,$00,$31,$6A,$21,$31,$17,$39
+	.byte	$14,$0A,$9A,$DA,$98,$D8,$0F,$07
+	.byte	$0C,$0C,$00,$03,$05,$05,$26,$46
+	.byte	$28,$28
+
+YMP_Electric_Piano_3:
+	.byte	$D4,$00,$31,$6A,$21,$31,$17,$39
+	.byte	$14,$0A,$9A,$DA,$98,$D8,$0F,$07
+	.byte	$0C,$0C,$00,$03,$05,$05,$26,$46
+	.byte	$28,$28
+
+YMP_Choir:
+	.byte	$D4,$00,$31,$6A,$21,$31,$17,$39
+	.byte	$14,$0A,$9A,$DA,$98,$D8,$0F,$07
+	.byte	$0C,$0C,$00,$03,$05,$05,$26,$46
+	.byte	$28,$28
+
+YMP_Crystal_1:
+	.byte	$D4,$00,$31,$6A,$21,$31,$17,$39
+	.byte	$14,$0A,$9A,$DA,$98,$D8,$0F,$07
+	.byte	$0C,$0C,$00,$03,$05,$05,$26,$46
+	.byte	$28,$28
+
+YMP_Brass:
+	.byte	$D4,$00,$31,$6A,$21,$31,$17,$39
+	.byte	$14,$0A,$9A,$DA,$98,$D8,$0F,$07
+	.byte	$0C,$0C,$00,$03,$05,$05,$26,$46
+	.byte	$28,$28
+
+
+.define PATCHTABLE YMP_marimba, YMP_finger_bass_1, YMP_Electric_Piano_1, YMP_Electric_Piano_2, YMP_Electric_Piano_3, YMP_Choir, YMP_Crystal_1, YMP_Brass
 
 patches_lo:   .lobytes   PATCHTABLE
 patches_hi:   .hibytes   PATCHTABLE
