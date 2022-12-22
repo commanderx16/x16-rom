997257346853576b6592fdd17e100b54b93a8f05
ZeroByteOrg <thatguy@zerobyte.org>
2022-12-22 10:41:49 -0600

    Allocated $AE00 as YM shadow RAM.

--- audio/memory.s
+++ audio/memory.s
@@ -12,3 +12,8 @@
 	azp0H   := azp0+1
 
 	azptmp: .res 1  ; single-byte TMP  (TODO: if we don't need this, remove it)
+
+; YM2151 is write-only. The library will keep a RAM shadow of writes in order
+; to facilitate functionalities like modifying the active values of the chip.
+.segment "SHADOW"
+	ymshadow: .res $ff
