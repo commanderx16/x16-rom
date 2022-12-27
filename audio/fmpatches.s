3ef1290550aa4a765e391c23ee653d1a459eccf7
mooinglemur <mooinglemur@users.noreply.github.com>
2022-12-26 20:43:25 -0700

    add python script that can extract patches from Furnace files in YMP format, prepare patches file for a large patch set

--- audio/fmpatches.s
+++ audio/fmpatches.s
@@ -9,55 +9,305 @@
 
 .segment "PATCHDATA"
 fm_patches:
-YMP_marimba:
-	.byte $DC,$00,$1B,$67,$61,$31,$21,$17,$1F,$0A,$DF,$5F,$DE
-	.byte $DE,$0E,$10,$09,$07,$00,$05,$07,$04,$FF,$A0,$16,$17
+YMP_Marimba:
+	.byte $DC,$00
+	.byte $1B,$67,$61,$31,$21,$17,$1F,$0A
+	.byte $DF,$5F,$DE,$DE,$0E,$10,$09,$07
+	.byte $00,$05,$07,$04,$FF,$A0,$16,$17
 
-YMP_finger_bass_1:
-	.byte	$F0,$00,$10,$10,$10,$10,$17,$17
-	.byte	$3F,$0A,$9E,$9C,$98,$9C,$0E,$04
-	.byte	$0A,$05,$08,$09,$09,$09,$B6,$C6
-	.byte	$C6,$C6
+YMP_Finger_Bass_1:
+	.byte $F0,$00
+	.byte $10,$10,$10,$10,$17,$17,$3F,$0A
+	.byte $9E,$9C,$98,$9C,$0E,$04,$0A,$05
+	.byte $08,$09,$09,$09,$B6,$C6,$C6,$C6
 
 YMP_Electric_Piano_1:
-	.byte	$D4,$00,$31,$6A,$21,$31,$17,$39
-	.byte	$14,$0A,$9A,$DA,$98,$D8,$0F,$07
-	.byte	$0C,$0C,$00,$03,$05,$05,$26,$46
-	.byte	$28,$28
+	.byte $D4,$00
+	.byte $31,$6A,$21,$31,$17,$39,$14,$0A
+	.byte $9A,$DA,$98,$D8,$0F,$07,$0C,$0C
+	.byte $00,$03,$05,$05,$26,$46,$28,$28
 
 YMP_Electric_Piano_2:
-	.byte	$DC,$00,$1F,$61,$61,$61,$28,$2A
-	.byte	$1E,$0A,$9F,$9E,$DB,$5E,$0F,$06
-	.byte	$07,$06,$08,$0B,$0A,$00,$8A,$F6
-	.byte	$86,$F7
+	.byte $DC,$00
+	.byte $1F,$61,$61,$61,$28,$2A,$1E,$0A
+	.byte $9F,$9E,$DB,$5E,$0F,$06,$07,$06
+	.byte $08,$0B,$0A,$00,$8A,$F6,$86,$F7
 
 YMP_Electric_Piano_3:
-	.byte	$FA,$00,$11,$44,$6C,$61,$21,$31
-	.byte	$47,$0A,$9C,$9C,$DB,$DA,$04,$04
-	.byte	$09,$03,$03,$03,$01,$00,$17,$06
-	.byte	$02,$A5
+	.byte $FA,$00
+	.byte $11,$44,$6C,$61,$21,$31,$47,$0A
+	.byte $9C,$9C,$DB,$DA,$04,$04,$09,$03
+	.byte $03,$03,$01,$00,$17,$06,$02,$A5
 
 YMP_Choir:
-	.byte	$C4,$00,$02,$62,$32,$62,$23,$23
-	.byte	$0A,$0A,$12,$12,$12,$12,$00,$00
-	.byte	$00,$00,$00,$00,$00,$00,$06,$06
-	.byte	$06,$06
+	.byte $C4,$00
+	.byte $02,$62,$32,$62,$23,$23,$0A,$0A
+	.byte $12,$12,$12,$12,$00,$00,$00,$00
+	.byte $00,$00,$00,$00,$06,$06,$06,$06
 
 YMP_Crystal_1:
-	.byte	$C4,$00,$07,$67,$02,$62,$18,$1C
-	.byte	$0A,$0A,$1F,$1F,$1F,$1F,$0C,$0E
-	.byte	$0C,$0C,$06,$06,$06,$06,$40,$40
-	.byte	$47,$47
+	.byte $C4,$00
+	.byte $07,$67,$02,$62,$18,$1C,$0A,$0A
+	.byte $1F,$1F,$1F,$1F,$0C,$0E,$0C,$0C
+	.byte $06,$06,$06,$06,$40,$40,$47,$47
 
 YMP_Brass:
-	.byte	$FA,$00,$62,$62,$26,$32,$19,$2A
-	.byte	$20,$0A,$8D,$15,$4F,$52,$06,$07
-	.byte	$08,$01,$02,$00,$00,$00,$18,$28
-	.byte	$18,$28
+	.byte $FA,$00
+	.byte $62,$62,$26,$32,$19,$2A,$20,$0A
+	.byte $8D,$15,$4F,$52,$06,$07,$08,$01
+	.byte $02,$00,$00,$00,$18,$28,$18,$28
+
+YMP_Flute:
+	.byte $CC,$00
+	.byte $00,$00,$00,$00,$29,$20,$09,$0E
+	.byte $1F,$1F,$13,$12,$05,$04,$04,$03
+	.byte $02,$02,$01,$0C,$32,$4D,$1D,$7B
+
+YMP_Silent:
+	.byte $C7,$00
+	.byte $00,$00,$00,$00,$7F,$7F,$7F,$7F
+	.byte $00,$00,$00,$00,$00,$00,$00,$00
+	.byte $00,$00,$00,$00,$00,$00,$00,$00
+
+.linecont +
+
+.define M0_PIANO \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Electric_Piano_3, \
+	YMP_Silent, \
+	YMP_Electric_Piano_1, \
+	YMP_Electric_Piano_2, \
+	YMP_Silent, \						
+	YMP_Silent
+
+.define M1_MALLET \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Marimba, \
+	YMP_Silent, \
+	YMP_Silent, \						
+	YMP_Silent
+
+.define M2_ORGAN \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \						
+	YMP_Silent
+
+.define M3_GUITAR \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \						
+	YMP_Silent
+
+.define M4_BASS \
+	YMP_Silent, \
+	YMP_Finger_Bass_1, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \						
+	YMP_Silent
+
+.define M5_STRINGS \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \						
+	YMP_Silent
+
+.define M6_ENSEMBLE \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \						
+	YMP_Silent
+
+.define M7_BRASS \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Brass, \						
+	YMP_Silent
+
+.define M8_REED \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \						
+	YMP_Silent
+
+.define M9_PIPE \
+	YMP_Silent, \
+	YMP_Flute, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \						
+	YMP_Silent
+
+.define MA_LEAD \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \						
+	YMP_Silent
+
+.define MB_PAD \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Choir, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \						
+	YMP_Silent
+
+.define MC_SYNFX \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Crystal_1, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \						
+	YMP_Silent
+
+.define MD_ETHNIC \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \						
+	YMP_Silent
+
+.define ME_PERC \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \						
+	YMP_Silent
+
+.define MF_SFX \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \						
+	YMP_Silent
 
 
+.define PATCHTABLE \
+	YMP_Marimba, \
+	YMP_Finger_Bass_1, \
+	YMP_Electric_Piano_1, \
+	YMP_Electric_Piano_2, \
+	YMP_Electric_Piano_3, \
+	YMP_Choir, \
+	YMP_Crystal_1, \
+	YMP_Brass, \
+	YMP_Flute, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent, \
+	YMP_Silent
 
-.define PATCHTABLE YMP_marimba, YMP_finger_bass_1, YMP_Electric_Piano_1, YMP_Electric_Piano_2, YMP_Electric_Piano_3, YMP_Choir, YMP_Crystal_1, YMP_Brass
+.linecont -
 
-patches_lo:   .lobytes   PATCHTABLE
-patches_hi:   .hibytes   PATCHTABLE
+patches_lo:
+	.lobytes PATCHTABLE
+patches_hi:
+	.hibytes PATCHTABLE
+
+; We're still using the old patch table for now
+
+patches_lo_:
+	.lobytes M0_PIANO
+	.lobytes M1_MALLET
+	.lobytes M2_ORGAN
+	.lobytes M3_GUITAR
+	.lobytes M4_BASS
+	.lobytes M5_STRINGS
+	.lobytes M6_ENSEMBLE
+	.lobytes M7_BRASS
+	.lobytes M8_REED
+	.lobytes M9_PIPE
+	.lobytes MA_LEAD
+	.lobytes MB_PAD
+	.lobytes MC_SYNFX
+	.lobytes MD_ETHNIC
+	.lobytes ME_PERC
+	.lobytes MF_SFX
+
+patches_hi_:
+	.hibytes M0_PIANO
+	.hibytes M1_MALLET
+	.hibytes M2_ORGAN
+	.hibytes M3_GUITAR
+	.hibytes M4_BASS
+	.hibytes M5_STRINGS
+	.hibytes M6_ENSEMBLE
+	.hibytes M7_BRASS
+	.hibytes M8_REED
+	.hibytes M9_PIPE
+	.hibytes MA_LEAD
+	.hibytes MB_PAD
+	.hibytes MC_SYNFX
+	.hibytes MD_ETHNIC
+	.hibytes ME_PERC
+	.hibytes MF_SFX
+
---
+++ audio/fur2ymp.py
@@ -0,0 +1,237 @@
+#!/usr/bin/env python3
+
+# Generation script for creating YM2151 patches for the
+# audio bank in Commander X16 KERNAL ROM
+# 
+# It parses .fur and .fui files for OPM and OPN instruments
+# and outputs "YMP" data as ca65 source to stdout
+#
+# by MooingLemur (c) 2022
+
+
+import sys
+import zlib
+import argparse
+import re
+from struct import unpack, pack
+
+instypes = [
+    '0: SN76489',
+    '1: FM (OPN)',
+    '2: Game Boy',
+    '3: C64',
+    '4: Amiga/sample',
+    '5: PC Engine',
+    '6: AY-3-8910',
+    '7: AY8930',
+    '8: TIA',
+    '9: SAA1099',
+    '10: VIC',
+    '11: PET',
+    '12: VRC6',
+    '13: OPLL',
+    '14: OPL',
+    '15: FDS',
+    '16: Virtual Boy',
+    '17: Namco 163',
+    '18: SCC',
+    '19: OPZ',
+    '20: POKEY',
+    '21: PC Speaker',
+    '22: WonderSwan',
+    '23: Lynx',
+    '24: VERA',
+    '25: X1-010',
+    '26: VRC6 (saw)',
+    '27: ES5506',
+    '28: MultiPCM',
+    '29: SNES',
+    '30: Sound Unit',
+    '31: Namco WSG',
+    '32: OPL (drums)',
+    '33: FM (OPM)',
+    '34: NES',
+    '35: MSM6258',
+    '36: MSM6295',
+    '37: ADPCM-A',
+    '38: ADPCM-B',
+    '39: SegaPCM',
+    '40: QSound',
+    '41: YMZ280B',
+    '42: RF5C68',
+    '43: MSM5232',
+    '44: T6W28',
+    '45: K007232',
+    '46: GA20',
+    '47: Pokémon Mini'
+]
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+def list_to_dotbyte_strings(lst):
+    result = []
+    for chunk in chunks(lst, 8):
+        result.append("\t.byte " + ','.join(['${:02X}'.format(int(x)) for x in chunk]) + "\n")
+    return result
+
+def extract_instrument_v127(ins):
+    format_version = unpack("<H",ins[0:2])[0]
+    instrument_type = unpack("<H",ins[2:4])[0]
+    instrument_name = "Untitled"
+    instrument_has_macros = False
+    instrument_affected_by_lfo = False
+    instrument_uses_eg = False
+
+    ympdata = [0]*26
+
+    if instrument_type == 1 or instrument_type == 33: # OPN, OPM
+        ins = ins[4:]
+        while len(ins) >= 4:
+            feature = ins[0:2]
+            feature_length = unpack("<H",ins[2:4])[0]
+            if feature == b'NA': # Instrument name
+                tmpnam = (ins[4:].split(b'\x00'))[0]
+                instrument_name = tmpnam.decode('utf-8')
+                if debug:
+                    sys.stderr.write("Instrument name is '{}'\n".format(instrument_name))
+                ins = ins[4+feature_length:]
+                continue
+            elif feature == b'MA': # Macros
+                if debug:
+                    sys.stderr.write("MA: Macros\n")
+                instrument_has_macros = True
+            elif feature == b'O1': # Macros
+                if debug:
+                    sys.stderr.write("O1: Macros\n")
+                instrument_has_macros = True
+            elif feature == b'O2': # Macros
+                if debug:
+                    sys.stderr.write("O2: Macros\n")
+                instrument_has_macros = True
+            elif feature == b'O3': # Macros
+                if debug:
+                    sys.stderr.write("O3: Macros\n")
+                instrument_has_macros = True
+            elif feature == b'O4': # Macros
+                if debug:
+                    sys.stderr.write("O4: Macros\n")
+                instrument_has_macros = True
+            elif feature == b'FM': # FM data, the meat
+                # [4] Flags: Skip 
+                # [5] ALG and FB.  Combine with RL turned on
+                tmp = ins[5]
+                ympdata[0] = (0xC0 | ((tmp & 0x07) << 3) | ((tmp & 0x70) >> 4))
+                # [6] PMS and AMS
+                tmp = ins[6]
+                ympdata[1] = (((tmp & 0x07) << 4) | ((tmp & 0x18) >> 3))
+                if (ympdata[1] & 0x70) > 0:
+                    instrument_affected_by_lfo = True
+                # [7] AM2|4|LLPatch: Skip
+                for i in range(4):
+                    x = i*8
+                    y = i*6
+                    # [8,16,24,32] DT1 and MUL
+                    tmp = ins[8+x]
+                    ympdata[2+y] = (tmp & 0x7F)
+                    # [9,17,25,33] TL
+                    tmp = ins[9+x]
+                    ympdata[3+y] = (tmp & 0x7F)
+                    # [10,18,26,34] KS and AR
+                    tmp = ins[10+x]
+                    ympdata[4+y] = (tmp & 0xDF)
+                    # [11,19,27,35] AMSEN and D1R
+                    tmp = ins[11+x]
+                    ympdata[5+y] = (tmp & 0x8F)
+                    if (ympdata[1] & 0x18) > 0 and (ympdata[5+y] & 0x80):
+                        instrument_affected_by_lfo = True
+                    # [12,20,28,36] D2R (we get DT2 later)
+                    tmp = ins[12+x]
+                    ympdata[6+y] = (tmp & 0x0F)
+                    if (tmp & 0x80) > 0:
+                        instrument_uses_eg = True
+                    # [13,21,29,37] D1L and RR
+                    tmp = ins[13+x]
+                    ympdata[7+y] = tmp
+                    # [14,22,30,38] Parameters not on OPM: Skip
+                    # [15,23,31,39] DT2
+                    tmp = ins[15+x]
+                    ympdata[6+y] = ympdata[6+y] | ((tmp & 0x18) << 3)
+
+            else:
+                if debug:
+                    sys.stderr.write("Skipping unsupported feature: '{}'\n".format(feature.decode('utf-8')))
+            ins = ins[4+feature_length:]
+            continue
+        print("\n; {}, Chip {}".format(instrument_name,instypes[instrument_type]))
+        if instrument_has_macros:
+            print("; WARNING: Instrument has macros")
+        if instrument_uses_eg:
+            print("; WARNING: Instrument uses OPN SSG-EG")
+        if instrument_affected_by_lfo:
+            print("; Instrument is affected by LFO")
+        print("{}:".format(re.sub("[^A-Za-z0-9]","_",instrument_name)))
+        print("".join(list_to_dotbyte_strings(ympdata[0:2])), end='')
+        print("".join(list_to_dotbyte_strings(ympdata[2:])), end='')
+
+    else:
+        if debug:
+            try:
+                sys.stderr.write("Skipping instrument type {}\n".format(instypes[instrument_type]))
+            except:
+                pass
+    return
+
+
+
+parser = argparse.ArgumentParser()
+inputgroup = parser.add_mutually_exclusive_group()
+inputgroup.add_argument("-i", metavar="input.fur", type=argparse.FileType('rb'), help='Input FUR/FUI file')
+
+args = parser.parse_args()
+
+debug = False
+
+if args.i is not None:
+    fur = args.i.read()
+
+    # zlib uncompress if it is compressed
+    try:
+        z = zlib.decompress(fur)
+        fur = z
+    except:
+        pass
+
+    while len(fur) > 0:
+        # Check for chunk type
+        if fur[0:16] == b'-Furnace module-':
+            if debug:
+                sys.stderr.write(".fur module\n")
+            fur = fur[32:]
+            continue
+        elif fur[0:4] == b'INS2':
+            if debug:
+                sys.stderr.write("INS2 (v127+ instrument)\n")
+            blocklen = unpack("<L",fur[4:8])[0]
+            extract_instrument_v127(fur[8:8+blocklen])
+            fur = fur[8+blocklen:]
+            continue
+        elif fur[0:4] == b'FINS':
+            if debug:
+                sys.stderr.write("FINS (v127+ standalone instrument)\n")
+            extract_instrument_v127(fur[4:])
+            fur = fur[len(fur):]
+            continue        
+        else:
+            if debug:
+                sys.stderr.write("Skipped {:s} Block\n".format(fur[0:4].decode('utf-8')))
+            blocklen = unpack("<L",fur[4:8])[0]
+            fur = fur[8+blocklen:]
+            continue
+ 
+
+
+
+    
